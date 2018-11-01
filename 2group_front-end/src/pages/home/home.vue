a287402eb24e07322a6c0fbaac52fb192e87583b
summary634 <sum690660932@163.com>
2018-11-01 09:49:34 +0800

    ll

---
+++ 2group_front-end/src/pages/home/home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="home">
+
+      <h3>这是home</h3>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'home'
+  }
+</script>
+
+<style>
+     #home{
+       width: 200px;
+       height: 200px;
+       background: red;
+       margin: 0 auto;
+     }
+</style>
